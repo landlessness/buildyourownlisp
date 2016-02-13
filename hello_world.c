623ed30cf0d8274cd3ba49f02ac36b2c7606e41c
landlessness <brian@landlessness.net>
2016-02-12 22:13:35 -0500

    hello world

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+void print_hello_times(int count) {
+  int i = 0;
+  do {
+    puts("i see you!");
+    i += 1;
+  } while (i < count);
+}
+
 int main(int argc, char** argv) {
-  puts("Hello, world!");
+  print_hello_times(10);
   return 0;
 }
